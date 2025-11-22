ef6cbd3bc748a2efed7f28bb1634bf056e7ade87
SOSYBOI <116269874+SOSYBOI@users.noreply.github.com>
2025-11-22 12:06:30 +0800

    Done adding pause menu. looking at skill tree.

--- Assets/Scripts/UI/SkillTree.cs
+++ Assets/Scripts/UI/SkillTree.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class SkillTree : MonoBehaviour
+public class SkillTree 
 {
-    // Start is called before the first frame update
-    void Start()
+    private SkillNode rootNode;
+    public class SkillNode
     {
-        
+        public string name;
+        private bool locked;
+        private bool unlockable;
+        private List<SkillNode> children;
+
+        SkillNode(string name, List<string> childrenNames , bool locked=true, bool unlockable = false)
+        {
+            this.name = name;
+            this.locked = locked;
+            this.unlockable = unlockable;
+            children = new List<SkillNode>(childrenNames.Count);
+            for(int i = 0;i < childrenNames.Count;i++)
+            {
+                children[i].name = childrenNames[i];
+            }
+        }
+
+        public bool IsLocked => locked;
+        public bool Unlockable => unlockable;
+        public void ApplyUpgrade(bool locked)
+        {
+            this.locked = locked;
+            if (!locked)
+            {
+                foreach(SkillNode node in children)
+                {
+                    node.SetUnlockable(true);
+                }
+            }
+        }
+
+        private void SetUnlockable(bool unlockable)
+        {
+            this.unlockable = unlockable;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
